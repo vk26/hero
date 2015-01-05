633e8b77a90cfae17eebacc044cded22cdb62de4
jFox <jfoxreality@gmail.com>
2015-01-05 17:19:12 +0000

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+User.create!([
+  {username: "demo", gender: "male", job: "novice", bio: nil, location: "new_zone03", lvl: 1, exp: 0, next: 660, hp: 6417, sp: 11, statpoint: 24, zeny: 1000, str: 1, agi: 1, vit: 1, int: 1, dex: 1, luk: 1, email: "demo@demo.com", encrypted_password: "$2a$10$4yDx0Roqv6Aui.wSiktf5.mc0sSLsjhtDIBSOg0kqIDW0V5lKCxxW", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-01-05 17:18:09", last_sign_in_at: "2015-01-05 17:18:09", current_sign_in_ip: "89.169.38.235", last_sign_in_ip: "89.169.38.235"}
+])
 Item.create!([
   {name: "knife", format: "weapon", price_sell: 25, weight: 40, attack: 17, defence: 0, slots: 4, jobs: "all", genders: "both", weapon_lvl: 1, refine: true, script: ""}
 ])
